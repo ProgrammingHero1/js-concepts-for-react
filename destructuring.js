476063aa8a8084dba5c2b76f8d1a3cacbbf9629c
Programming Hero <53802153+ProgrammingHero1@users.noreply.github.com>
2023-03-11 21:45:41 -0600

    Add files via upload

--- destructuring.js
+++ destructuring.js
@@ -1,39 +1,53 @@
-const numbs = [65, 23];
-const [a, b] = [65, 23];
-const [x, y] = numbs;
+// 1. array destructuring
+const numbers = [42, 65];
+// const x = numbers[0];
+// const y = numbers[1];
 
-const boxified = (num1, num2) => {
-    const numbers = [num1, num2];
-    return numbers;
+// const [x, y] = [42, 65]
+
+
+const [x, y] = numbers;
+
+function boxify(num1, num2) {
+    const nums = [num1, num2];
+    return nums;
 }
 
-const result = boxified(11, 67);
-// console.log(result);
+// const [first, second] = [90, 34]
+const [first, second] = boxify(90, 34);
 
-const [members, paidMember] = boxified(56, 45);
-// console.log(members, paidMember);
-
-
-// object 
+// console.log(boxify(90, 34));
 const student = {
-    name: 'Seriously Fakibaj',
-    profession: 'Student',
-    languages: ['bangla', 'hindi', 'english'],
-    address: 'kochu land',
-    friend: {
-        name: 'sakib al hasan',
-        job: 'cricket',
-        hobby: 'advertisement'
+    name: 'Salib Khan',
+    age: 32,
+    movies: ['king khan', 'Dhakar Mastan']
+}
+
+const [firstMovie, secondMovie] = student.movies;
+
+
+// object destructuring
+const { name, age } = { name: 'alu', age: 14 };
+const { name, age } = { id: 12, name: 'alu', salary: 3400, age: 14 };
+
+const employee = {
+    ide: 'VS Code',
+    designation: 'developer',
+    machine: 'mac',
+    languages: ['html', 'css', 'js'],
+    specification: {
+        height: 66,
+        weight: 67,
+        address: 'kumarkhali',
+        drink: 'water',
+        watch: {
+            color: 'black',
+            price: 500,
+            brand: 'garmin'
+        }
     }
 }
 
-const {name, age} = {name: 'JOmir uddin', age: 32, money: 562}
-// console.log(name);
-
-const {profession, address} = student;
-
-const [firstLanguage] = student?.languages;
-const {hobby} = student?.friend;
-
-console.log(firstLanguage, hobby);
-
+const { machine, ide } = employee;
+// const { weight, address } = employee.specification;
+const { brand } = employee?.specification?.watch;
--- notation.js
+++ notation.js
@@ -1,16 +1,21 @@
 const person = {
-    name: 'Jomir Uddin jomidar',
-    age: 51,
-    profession: 'jomi dhar',
-    children: ['abc', 'xyz'],
-    23: 'sigle'
+    name: 'Lal Nil Holud mia',
+    profession: 'Traffic Surgeon', 
+    age: 39,
+    25: 'Summer',
+    'son-name': 'Sunny',
+    address: 'Shahabag'
 }
 
 // dot notation
 const prof1 = person.profession;
 
 // bracket notation
-const prof2 = person['profession']
-const profName = 'profession';
-const prof3 = person[profName]
-const num = person['23']
+const prof2 = person["profession"];
+const pName = 'profession';
+const prof3 = person[pName];
+// console.log(prof3);
+
+const season = person[25];
+// console.log(season);
+const son = person['son-name'];
